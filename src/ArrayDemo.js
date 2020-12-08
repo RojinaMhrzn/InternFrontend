adcc0a5f23f98750357d282e8601d623fa1ee0b0
Rojina Maharjan <rojina.maharjan@gurzu.com>
2020-12-08 14:05:03 +0545

    First Commit

---
+++ src/ArrayDemo.js
@@ -0,0 +1,18 @@
+
+
+function ArrayDemo(){
+    const m = `This is ArrayDemo Page`;
+        return(
+            <div>
+                <h3>{m}</h3>
+            </div>
+        );
+}
+export default ArrayDemo;
+
+
+    
+
+    
+    
+
